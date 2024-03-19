628f2af2705a7710538c52d975d772becb30a689
ftmhabt <ftmh.a78@gmail.com>
2024-03-20 00:07:59 +0330

    Add Profile & App code

---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+const Profile = () => {
+    return (
+      <div>
+        <h1>Hello from profile page!</h1>
+        <p>So, how are you?</p>
+      </div>
+    );
+  };
+  
+export default Profile;
+  
