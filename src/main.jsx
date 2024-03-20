784fc28c92d120f010defea5b3a15c97bf68787e
ftmhabt <ftmh.a78@gmail.com>
2024-03-20 20:20:01 +0330

    Add error page and router files

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
-import Profile from "./Profile";
-import DefaultProfile from "./DefaultProfile";
-import Spinach from "./Spinach";
-import Popeye from "./Popeye";
+import Router from "./Router";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "profile",
-    element: <Profile />,
-    children: [
-      { index: true, element: <DefaultProfile /> },
-      { path: "spinach", element: <Spinach /> },
-      { path: "popeye", element: <Popeye /> },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router/>
   </React.StrictMode>
 );
